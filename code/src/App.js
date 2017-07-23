b09a9ea5f4e4a7ac52f399acbe2f39a82a386931
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-07-23 17:32:51 -0400

    pokemon names loading

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonArray: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    const xhrRequest = new XMLHttpRequest();
+    xhrRequest.open('GET', 'http://pokeapi.co/api/v2/pokemon/');
+    xhrRequest.send();
+    xhrRequest.onreadystatechange = () => {
+      let data;
+        if (xhrRequest.readyState === 4 && xhrRequest.status === 200) {
+          data = JSON.parse(xhrRequest.responseText);
+          this.setState({ pokemonArray: data.results });
+        }
+    }
+  }
+
+  createPokemonElements() {
+      return (
+      <div className='pokemonNames'>
+          <ul>
+              {this.state.pokemonArray.map((pokemon, i) => {
+                return <li key={i}>{pokemon.name}</li>
+              })}
+          </ul>
+        </div>
+        );
+    };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="pokeList">
+          <h3>Pokemon</h3>
+          { this.createPokemonElements() }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      );
   }
 }
 
